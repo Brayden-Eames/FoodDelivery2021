f7dce5dcfa164dfd4070e854ae50d2b89289b9e4
Brayden Eames <braydeneames@ymail.com>
2021-08-30 16:45:39 -0600

    Add project files.

---
+++ FoodDelivery/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FoodDelivery.Pages;
+
+namespace FoodDelivery.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FoodDelivery.Pages.Category> Category { get; set; }
+    }
+}
